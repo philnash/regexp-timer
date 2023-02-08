93c795ca087713a1851b7f020b39e584a7341ccf
Phil Nash <philnash@gmail.com>
2023-02-08 16:16:52 +1100

    Adds some examples

--- static/app.js
+++ static/app.js
@@ -1,42 +1,16 @@
 const form = document.getElementById("regex-tester");
 const regexInput = document.getElementById("regex");
 const stringInput = document.getElementById("test-string");
+const testButton = document.getElementById("test-button");
 const testOutput = document.getElementById("test-output");
 
 let start,
   interval,
-  calculating = false;
+  calculating = false,
+  stopButton = createButton(),
+  worker = createRegexWorker();
 
-function createRegexWorker() {
-  const worker = new Worker("/static/regexWorker.js");
-  worker.addEventListener("message", (event) => {
-    clearInterval(interval);
-    const end = performance.now();
-    const result = event.data;
-    testOutput.innerText = `The result is ${result} and it was calculated in ${
-      end - start
-    }ms`;
-    calculating = false;
-  });
-
-  worker.addEventListener("error", (event) => {
-    clearInterval(interval);
-    const end = performance.now();
-    console.log(event);
-    testOutput.innerText = `The test failed with the message "${
-      event.message
-    }", and it took ${end - start}ms`;
-    calculating = false;
-  });
-  return worker;
-}
-
-let worker = createRegexWorker();
-
-function updateTimer() {
-  const now = performance.now();
-  testOutput.innerText = `${now - start}ms`;
-}
+turnExamplesIntoButtons();
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -45,12 +19,91 @@ form.addEventListener("submit", (event) => {
     const testRegex = regexInput.value.trim();
     const testString = stringInput.value.trim();
     if (testRegex && testString) {
-      start = performance.now();
-      interval = setInterval(updateTimer, 50);
-      worker.postMessage({
-        regex: testRegex,
-        string: testString,
-      });
+      testRegexAgainstString(testRegex, testString);
     }
   }
 });
+
+function createRegexWorker() {
+  const worker = new Worker("/static/regexWorker.js");
+  worker.addEventListener("message", successfulMatch);
+  worker.addEventListener("error", unsuccessfulMatch);
+  return worker;
+}
+function createButton(text = "Stop") {
+  const button = document.createElement("button");
+  button.textContent = text;
+  return button;
+}
+function updateTimer() {
+  testOutput.innerText = `${Math.round(performance.now() - start)}ms`;
+}
+
+function terminateWorker() {
+  clearInterval(interval);
+  calculating = false;
+  worker.terminate();
+  stopButton.remove();
+  testOutput.innerText = `The test was terminated without result after ${Math.round(
+    performance.now() - start
+  )}ms`;
+  stopButton = createButton();
+  worker = createRegexWorker();
+}
+
+function successfulMatch(event) {
+  clearInterval(interval);
+  const result = event.data;
+  stopButton.remove();
+  stopButton = createButton();
+  testOutput.innerText = `The result is ${result} and it was calculated in ${Math.round(
+    performance.now() - start
+  )}ms`;
+  calculating = false;
+}
+
+function unsuccessfulMatch(event) {
+  clearInterval(interval);
+  stopButton.remove();
+  stopButton = createButton();
+  console.log(event);
+  testOutput.innerText = `The test failed with the message "${
+    event.message
+  }", and it took ${Math.round(performance.now() - start)}ms`;
+  calculating = false;
+}
+
+function testRegexAgainstString(testRegex, testString) {
+  testButton.insertAdjacentElement("afterend", stopButton);
+  stopButton.addEventListener("click", terminateWorker);
+  start = performance.now();
+  interval = setInterval(updateTimer, 50);
+  worker.postMessage({
+    regex: testRegex,
+    string: testString,
+  });
+}
+
+function turnExamplesIntoButtons() {
+  const listItems = document.querySelectorAll(".example-list li");
+  console.log(listItems);
+  Array.from(listItems).forEach((listItem) => {
+    const button = createButton(listItem.textContent);
+    button.addEventListener(
+      "click",
+      fillFormWithExample(
+        listItem.textContent,
+        listItem.dataset["exampleString"]
+      )
+    );
+    listItem.innerHTML = "";
+    listItem.appendChild(button);
+  });
+}
+
+function fillFormWithExample(regex, string) {
+  return function () {
+    regexInput.value = regex;
+    stringInput.value = string;
+  };
+}
